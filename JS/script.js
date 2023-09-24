e6ce0985dd9c601a1875bfa77874ab722fc8d933
Suprio-Das <priyamd507.gmail.com>
2023-09-25 00:03:55 +0600

    API Connected and Displayed

--- JS/script.js
+++ JS/script.js
@@ -7,5 +7,23 @@ const loadCountries = () =>{
 loadCountries();
 
 const displayCountries = countries => {
-    
+    const countriesContainer = document.getElementById('countriesContainer');
+    const countriesDiv = document.getElementById('countriesDiv');
+
+    for(const country of countries){
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="col">
+        <div class="card h-100">
+          <img src="${country.flags.png}" class="card-img-top img-fluid" alt="..." style="height: 200px">
+          <div class="card-body">
+            <p class="card-text">Name: ${country.name.common}</p>
+            <p class="card-text">Capital: ${country.capital}</p>
+            <p class="card-text">Population: ${country.population}</p>
+          </div>
+        </div>
+      </div>
+        `
+        countriesContainer.appendChild(div);
+    }
 }
